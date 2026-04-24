fa70e3147f54db55e1939b0807e84c10f32d9902
stasyzar <zarovskaanatasia@gmail.com>
2026-04-24 12:35:01 +0300

    add axios instance with request interceptor for authorization

---
+++ src/api/axiosInstance.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:8080/api',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+});
+
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('accessToken');
+        if(token){
+            if(config.headers){
+                config.headers.Authorization = `Bearer ${token}`;
+            }
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default axiosInstance;
